814e5cb873f07546c94ee953bb5564f5fa6b8401
Andrew Steele <andrew565@gmail.com>
2012-08-23 14:32:41 -0400

    All of the necessary integration tests.
    And... I'm done.

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -5,4 +5,16 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
 		post "/checker", :num => 3
 		assert_equal('Fizz', response.body)
 	end
+	test "should get 'Buzz' when posting 5 as num value" do
+		post "/checker", :num => 5
+		assert_equal('Buzz', response.body)
+	end
+	test "should get 'FizzBuzz' when posting 30 as num value" do
+		post "/checker", :num => 30
+		assert_equal('FizzBuzz', response.body)
+	end
+	test "should get '44' when posting 44 as num value" do
+		post "/checker", :num => 44
+		assert_equal('44', response.body)
+	end
 end
