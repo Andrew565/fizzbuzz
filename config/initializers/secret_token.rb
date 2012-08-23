9bbbca44571819cdcead3bb41e3ef7a9efe41375
Andrew Steele <andrew565@gmail.com>
2012-08-23 11:24:56 -0400

    Installed Rails, moved FizzBuzz to lib directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FizzBuzz::Application.config.secret_token = '081a9bd41dfba8e27b220f6e89b39cf8a09e11929a9b0bad2101311ea0d0efcc42a12af05ffd0c6a49c160ddce7639b1905b3200640f22f8260492b4c6938e00'
